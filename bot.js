132cdf0a6dc95f2f773e81a6a29590d7b8a9cc80
daThug <40901028+daThug@users.noreply.github.com>
2020-06-09 17:05:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,18 @@ client.on('message', message => {
   	}
 });
 
+client.on("ready", () => {
+    const channel = client.channels.cache.get("ChannelIDhere");
+    if (!channel) return console.error("The channel does not exist!");
+    channel.join().then(connection => {
+        // Yay, it worked!
+        console.log("Successfully connected.");
+    }).catch(e => {
+
+        // Oh no, it errored! Let's log it to console :)
+        console.error(e);
+    });
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
